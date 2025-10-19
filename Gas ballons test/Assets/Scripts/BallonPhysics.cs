c0c390c237001954ca2a52b1faf1d411c3be0121
IamCoVIK <xaxa12369@gmail.com>
2025-10-19 22:21:36 +0700

    баллон бум при ударе

--- Gas ballons test/Assets/Scripts/BallonPhysics.cs
+++ Gas ballons test/Assets/Scripts/BallonPhysics.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class BallonPhysics : MonoBehaviour
 {
     public Rigidbody rb;
     public AudioSource hitSound;
 
+    public UnityEvent hardHit;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (rb.velocity.magnitude > 1f)
         {
             hitSound.Play();
+            if (rb.velocity.magnitude > 3f)
+            {
+                hardHit.Invoke();
+            }
         }
     }
 }
